eb3cc0ac98fdf7b02c7d5a21119028c55a1342dc
Hiromi OHKUBO <158258166+ptbio-ohkubo@users.noreply.github.com>
2026-04-09 15:55:18 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://ptbio-ohkubo.github.io',
+  base: '/egg-for-all',
+});
