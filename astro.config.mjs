4fdd444249973bd795c730cd843281c26eec5c5f
Hiromi OHKUBO <m.ohkubo@arc-design-lab.com>
2026-04-10 16:26:31 +0900

    [fix] gitignore修正

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-  site: 'https://ptbio-ohkubo.github.io',
-  base: '/egg-for-all',
-});
