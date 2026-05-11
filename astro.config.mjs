b44e50495dd975a373b55fd9111e3754299444c1
Hiromi OHKUBO <m.ohkubo@arc-design-lab.com>
2026-05-11 14:20:51 +0900

    [feat] サイト準備

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,34 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import icon from 'astro-icon';
+
+// astro.config.mjs がある場所を基点にして SCSS パーシャルのパスを絶対化
+const projectRoot = fileURLToPath(new URL('.', import.meta.url));
+const sassPath = path.resolve(projectRoot, 'src/assets/sass');
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ptbio-ohkubo.github.io',
-  base: '/egg-for-all',
+  site: 'http://localhost:4321/',
+  base: '/egg-for-all/',
+  integrations: [icon()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "_base" as base;
+            @use "_font" as font;
+            @use "_mixin" as mixin;
+            @use "_reset" as reset;
+            @use "_variables" as vars;
+          `,
+          loadPaths: [sassPath],
+        }
+      }
+    }
+  },
+  devToolbar: {
+    enabled: false,
+  },
 });
